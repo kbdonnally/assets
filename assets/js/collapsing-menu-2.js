cc76e52646d4130584937f0785eac5a705b60759
Katherine Donnally <kbd3ax@virginia.edu>
2019-12-23 11:05:33 -0500

    Get the display of which links working in the nav page

---
+++ assets/js/collapsing-menu-2.js
@@ -0,0 +1,36 @@
+const windowHalfWidth = window.innerWidth * .75;
+const listOne 		  = document.querySelectorAll('.collapse-menu__list-item--1');
+const listTwo 		  = document.querySelectorAll('.collapse-menu__list-item--2');
+const widthsOne 	  = [...listOne].map(x => x.offsetWidth);
+
+function getIndex(listOne, windowHalfWidth) {
+	let counter = 0;
+	for (const [index, item] of listOne.entries()) {
+		counter += item.offsetWidth;
+		if (counter > windowHalfWidth) {
+			console.log(`width: ${counter}; index: ${index}`);
+			console.log(index);
+			return index - 1;
+		}
+	}
+}
+
+const index 		= getIndex(listOne, windowHalfWidth); // index to split the links
+const listOneSlice  = [...listOne].slice(index); // slice of array from index to end
+console.log(listOneSlice);
+
+// hide from List 1
+for (const item of listOneSlice) {
+	item.classList.add('collapse-menu--to-hide'); // hide items outside of width range
+}
+
+// List 2 activity //
+
+const listTwoHideSlice = [...listTwo].slice(0, index + 1);
+const listTwoShowSlice = [...listTwo].slice(index);
+console.log('List two hiding slice: ' + listTwoHideSlice);
+
+// hide from List 1
+for (const item of listTwoHideSlice) {
+	item.classList.add('collapse-menu--to-hide'); // hide items outside of width range
+}
--- assets/js/collapsing-menu.js
+++ assets/js/collapsing-menu.js
@@ -1,22 +1,47 @@
 /* To go with nav-menu--collapse.html */
 
 let windowWidth = window.innerWidth;
-let listWidth = .75 * window.innerWidth;
+let partialWidth = .75 * window.innerWidth;
 const childrenOne = document.querySelector('.collapse-menu__list-1').children;
 const widthsOne = [...childrenOne].map(x => x.offsetWidth);
 
-let totalWidth = 0;
+
 console.log(widthsOne);
+console.log('Partial width: ' + partialWidth);
 
-for (let [i, width] of widthsOne.entries()) {
-	if (totalWidth < listWidth) {
-		console.log(i, width);
+let totalWidth = 0;
+
+const itemsWidth = () => {
+	for (let [i, width] of widthsOne.entries()) {
+		if (totalWidth >= partialWidth ) {
+			var itemNum = i - 1;
+			console.log(totalWidth, width, itemNum);
+			const myWidth = totalWidth - width;
+			return [itemNum, myWidth, width];
+		}
+		totalWidth += width;
 	}
-	if (totalWidth >= listWidth) {
-	//	return width.index;
-		console.log(width, totalWidth);
+};
+
+const itemNum = itemsWidth()[0] - 1;
+const myWidth = itemsWidth()[1];
+const itemWidth = itemsWidth()[2];
+
+console.log('Number item: ' + itemNum);
+console.log('Total width: ' + myWidth);
+// console.log('Last item width: + ' itemWidth);
+
+// Okay, so items 5 to x need to be hidden
+
+const childrenToHide = [...childrenOne];
+console.log(childrenToHide[2, 5]);
+
+for (let [i, item] of childrenToHide.entries()) {
+	if (i >= itemNum) {
+		item.classList.add('test');
+		item.classList.add('collapse-menu--to-hide');
 	}
-	totalWidth += width;
+	if (i < itemNum) {
+		item.classList.add('test-2');
+	}
 }
-
-console.log(totalWidth);
