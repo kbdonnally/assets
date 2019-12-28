54eaa01c0f1b6cfa2878d9bc239ab14f28b2d0e7
Katherine Donnally <kbd3ax@virginia.edu>
2019-12-28 18:07:16 -0500

    Add page to make functionality from scratch

---
+++ assets/js/menu-practice-2.js
@@ -0,0 +1,35 @@
+/* Goes with menu-practice-2.html */
+
+const navMenusWrapper = document.querySelector('.nav__list-holder');
+const navListOne = navMenusWrapper.children[0];
+const navListTwo = navMenusWrapper.children[1];
+const morePrompt = document.querySelector('.nav-list__more-prompt');
+
+// widths
+let moreWidth = morePrompt.offsetWidth;
+let menuWidth = navMenusWrapper.offsetWidth;
+let availableWidth = menuWidth - moreWidth;
+const menuOneItems = navListOne.children;
+const menuTwoItems = navListTwo.children;
+const menuOneWidths = Array.from(navListOne.children, item => parseInt(item.offsetWidth));
+
+let i = -1;
+let sumWidth = 0;
+
+while (sumWidth < availableWidth) {
+	i++;
+	sumWidth += menuOneWidths[i];
+}
+
+const splitIndex = i;
+
+for (i; i < (menuOneItems.length); i++) {
+	menuOneItems[i].classList.add('hidden--js');
+}
+
+for (var j=1; j < splitIndex; j++) {
+	if (document.querySelector('.nav-list-2__item')) {
+		menuTwoItems[j].classList.add('hidden--js');	
+	}
+	
+}
